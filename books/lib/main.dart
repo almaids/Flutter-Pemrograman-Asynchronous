d2f8c6708d24ef187f79a9d9712ee1d3a294f117
almaids <150983302+almaids@users.noreply.github.com>
2025-03-22 18:06:17 +0700

    W5: Soal 6

--- books/lib/main.dart
+++ books/lib/main.dart
@@ -76,9 +76,20 @@ class _FuturePageState extends State<FuturePage> {
   return completer.future;
   }
 
-  Future calculate() async {
-  await Future.delayed(const Duration(seconds: 5));
-  completer.complete(42);
+  // Future calculate() async {
+  // await Future.delayed(const Duration(seconds: 5));
+  // completer.complete(42);
+  // }
+
+  calculate() async {
+    try {
+      await new Future.delayed(const Duration(seconds: 5));
+      completer.complete(42);
+  // throw Exception();
+    }
+    catch (_) {
+      completer.completeError({});
+    }
   }
 
 
@@ -120,11 +131,18 @@ class _FuturePageState extends State<FuturePage> {
           ElevatedButton(
             child: Text('GO!'),
             onPressed: (){
-              getNumber().then((value) {
-                            setState(() {
-                              result = value.toString();
-                            });
-                            });
+            //  getNumber().then((value) {
+            //                setState(() {
+            //                  result = value.toString();
+            //                });
+            //                });
+            getNumber().then((value) {
+              setState(() {
+                result = value.toString();
+              });
+            }).catchError((e) {
+              result = 'An error occurred';
+            });
             },
           ),
 
